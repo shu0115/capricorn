9711e84c084fbef5e9915708fd406aa8779a6dc4
Shun Matsumoto <raisondetre0115@gmail.com>
2011-03-19 20:45:34 +0900

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,16 +33,19 @@ class ApplicationController < ActionController::Base
   #--------------#
   def ssl_redirect
     # httpへリダイレクト
-    if Rails.env.production? and request.env["HTTP_X_FORWARDED_PROTO"].to_s == "https" and session[:user_id].blank?
+    if Rails.env.production? and request.env["HTTP_X_FORWARDED_PROTO"].to_s == "https" and ( params[:controller] == "tasks" and params[:action] == "index" ) and session[:user_id].blank?
       request.env["HTTP_X_FORWARDED_PROTO"] = "http"
+      redirect_to request.url and return
+    elsif Rails.env.production? and request.env["HTTP_X_FORWARDED_PROTO"].to_s != "https"
+      request.env["HTTP_X_FORWARDED_PROTO"] = "https"
       redirect_to request.url and return
     end
 
     # httpsへリダイレクト
-    if Rails.env.production? and request.env["HTTP_X_FORWARDED_PROTO"].to_s != "https" and !(session[:user_id].blank?)
-      request.env["HTTP_X_FORWARDED_PROTO"] = "https"
-      redirect_to request.url and return
-    end
+    #    if Rails.env.production? and request.env["HTTP_X_FORWARDED_PROTO"].to_s != "https" and !(session[:user_id].blank?)
+    #  request.env["HTTP_X_FORWARDED_PROTO"] = "https"
+    #  redirect_to request.url and return
+    #end
   end
 
 end
