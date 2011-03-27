a2355ca7a8048e8d2a9c063c6cc2bbbdfee8d5e9
Shun Matsumoto <raisondetre0115@gmail.com>
2011-03-27 17:40:32 +0900

    update

--- app/models/task.rb
+++ app/models/task.rb
@@ -27,7 +27,7 @@ class Task < ActiveRecord::Base
 
     condition_text = "user_id = #{args[:user_id].to_i}"
     condition_hash = Hash.new
-    order = "status DESC, created_at DESC, complete_date DESC"
+    order = "status DESC, complete_date DESC, created_at DESC"
 
     # カテゴリ
     unless args[:search][:category].blank?
